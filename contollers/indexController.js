4c2971e175e40d85a1f7b1f0e080c098a4b7dd0d
coccoto <soprano.lune@gmail.com>
2020-08-10 05:16:17 +0900

    2020-08-10 05:16

--- contollers/indexController.js
+++ contollers/indexController.js
@@ -1,24 +1,26 @@
 const EscapeModel = require(ROOT + '/models/escapeModel')
 
 module.exports = (req, res) => {
-    const header = () => {
+    const setHeader = () => {
         res.header('Access-Control-Allow-Origin', '*')
         res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
     }
 
     const escape = () => {
         const source = req.body.source
+
         console.log('source: ' + source)
+
         return EscapeModel(source)
     }
 
     const main = () => {
+        setHeader()
         const result = escape()
+
         console.log('result: ' + result)
-        header()
-        res.json({
-            source: result
-        })
+
+        res.json({ source: result })
     }
     return main()
 }
